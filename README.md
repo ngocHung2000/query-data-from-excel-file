8804fe941f414209c7e9bd1b805d2301c4777d21
Ngọc Hùng Tô <tongochung1809@gmail.com>
2024-05-01 23:59:47 +0700

    hungtn29

---
+++ README.md
@@ -0,0 +1,8 @@
+Before you can install libraries on your vm
+```
+python3 -m pip install -r requirements.txt
+```
+Run app
+```
+python my_script.py
+```
---
+++ scripts.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+while read line
+do
+    NAMESPACE=$(echo $line | cut -d ' ' -f 1)
+    INSTANCE_NAME=$(echo $line | cut -d ' ' -f 2)
+    MAX_REPLICAS=$(echo $line | cut -d ' ' -f 3)
+    cat <<EOF >> output.yaml
+---
+Instance ${INSTANCE_NAME}
+Replicas ${MAX_REPLICAS}
+EOF
+done < output.txt
